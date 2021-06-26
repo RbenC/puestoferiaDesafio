78af66e09690e618d4bf079f2e54683097e53bbe
José Rubén Cerrada <economiayexcel@gmail.com>
2021-06-25 21:36:51 -0400

    Actualizacion con async await  Throw Error

--- index.js
+++ index.js
@@ -73,19 +73,37 @@ const stock =  [
         "unidad": "Kg"
     }
 ]
+
 let cantidadMinima = 5; // Alerta para subir el precio de los productos cuando quede poca existencia (modificar el precio de un producto cuando algún producto tenga una existencia).
 let incrementoPrecio = 1.3; // Incrementar 30%  Al llegar a 5 como cantidad minima en stock del producto 
-let productoComprar ='Papa';  // Producto a comprar 
-let cantidadComprar = 6; // Cantidad del producto a comprar 
+const productoComprar ='Papa';  // Producto a comprar 
+const cantidadComprar = 6; // Cantidad del producto a comprar 
 let mensaje='Gracias por su compra. '; 
 let totalPagar = 0;
 let totalValorStock=0;
+let stockDisponible = []; 
 
 //************************************************* */ //Cantidad total de productos  con existencia > 0
-const stockDisponible = stock.filter((item)=>{
-    return  item.cantidad>0;
-});
+function productosDisponiblesMayoraCero() {
+    return new Promise(resolve => {              
+            resolve(            
+                stock.filter((item)=>{
+                    return item.cantidad > 0;
+            }))        
+    });
+  }
+  
+  async function asyncCall() {
+    console.log('Llamando a los Productos que tengan cantidad mayor a cero ');
+    stockDispoonible = await productosDisponiblesMayoraCero();
+    console.log(stockDispoonible);
+}
+
+asyncCall();   // Llamada asíncrona 
 //************************************************* */
+
+
+
 //************************************************* */ // Cantidades de existencia > 0 y < 5 
 const stockDisponible2 = stock.filter((item)=>{
     return  item.cantidad > 0 && item.cantidad <= cantidadMinima 
@@ -105,10 +123,14 @@ const posicion = stock.findIndex((item)=>{
 switch (productoComprar) {
     case 'Uva':
         if(stock[posicion].cantidad>cantidadComprar){
-            totalPagar=cantidadComprar * stock[posicion].precio;
-            mensaje+= `Total = ${cantidadComprar} * ${stock[posicion].precio} = ${new Intl.NumberFormat("de-DE").format(totalPagar)}`; 
+            totalPagar=cantidadComprar * stock[posicion].precio;            
         } else {
-            mensaje+= `Lamentamos no disponer la cantidad solicitada de ${productoComprar} que requiere. `
+            try {
+                throw new Error(`Total = ${cantidadComprar} * ${stock[posicion].precio} = ${new Intl.NumberFormat("de-DE").format(totalPagar)}`)
+            } catch (error) {                
+                // mensaje+= `Lamentamos no disponer la cantidad solicitada de ${productoComprar} que requiere. `
+                console.log(error);
+            }
         }
         break;
     case 'Manzana Verde':
@@ -120,7 +142,7 @@ switch (productoComprar) {
         }
         break;
     case 'Manzana Roja':
-        if(stock[posicion].cantidad>cantidadComprar){
+        if(stock[posicion].cantidad > cantidadComprar){
             totalPagar=cantidadComprar * stock[posicion].precio;
             mensaje+= `Total = ${cantidadComprar} * ${stock[posicion].precio} = ${new Intl.NumberFormat("de-DE").format(totalPagar)}`; 
         } else {
@@ -218,12 +240,12 @@ console.log(`Puesto de Feria Santaolalla - ${stock.length} Productos `);
 console.log('************************************************************************************')
 
 
-console.log();
-console.log('************************************************************************************')
-console.log(`Puesto de Feria Santaolalla -  ${stockDisponible.length} Productos en Stock Disponibles Cantidades > 0 ` );
-console.log(stockDisponible);
-console.log('************************************************************************************')
-console.log();
+// console.log();
+// console.log('************************************************************************************')
+// console.log(`Puesto de Feria Santaolalla -  ${stockDisponible.length} Productos en Stock Disponibles Cantidades > 0 ` );
+// console.log(stockDisponible);
+// console.log('************************************************************************************')
+// console.log();
 
 console.log();
 console.log('************************************************************************************')
@@ -251,3 +273,4 @@ console.log('*******************************************************************
 console.log(`Valor Total del Stock ${new Intl.NumberFormat("de-DE").format(totalValorStock)} CLP`);
 console.log('************************************************************************************')
 console.log();console.log();console.log();
+
